5947ef62e5bd96164235b80329980e3e7df5e7e8
Dan Puiatti <dpuiatti@gmail.com>
2019-02-25 19:45:05 -0500

    rebuilt page

---
+++ development/src/components/Blog.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Blog = () => {
+    return (
+
+<div>
+
+
+    <p>Blog</p>
+</div>
+
+
+
+    );
+};
+
+export default Blog;
---
+++ development/src/components/Portfolio.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Portfolio = () => {
+    return (
+
+<div>
+
+
+    <p>Portfolio</p>
+</div>
+
+
+
+    );
+};
+
+export default Portfolio;
