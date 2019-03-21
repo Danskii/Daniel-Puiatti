a8d7daf55d7b2a574a63e7f5861afb422cc95b02
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-03-21 10:12:25 -0400

    add

---
+++ development/build/precache-manifest.14fdaaa1344ad044082df85025396d37.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57700483cfdf8fc4f95bbc48b1e7d09",
+    "url": "/static/media/daniel-puiatti.e5770048.jpg"
+  },
+  {
+    "revision": "990e3d32daec91ace05e",
+    "url": "/static/css/main.013adf7d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ca8ac8a298c44f7dd5ea",
+    "url": "/static/js/2.ca8ac8a2.chunk.js"
+  },
+  {
+    "revision": "eddc245876ce31db54752f1d0c445898",
+    "url": "/static/media/professionally-speaking.eddc2458.jpg"
+  },
+  {
+    "revision": "43d8fb4540d662655cd506e35db06ed3",
+    "url": "/static/media/daniel-puiatti-photography.43d8fb45.jpg"
+  },
+  {
+    "revision": "0dfe5013c38a11ce5c36473c215344df",
+    "url": "/static/media/toronto-bike-fixit-map.0dfe5013.jpg"
+  },
+  {
+    "revision": "92a3d6a2ca0a361f4177f8109cfdcaab",
+    "url": "/static/media/danskii.92a3d6a2.jpg"
+  },
+  {
+    "revision": "2176112803a38a385e3cce50322a6372",
+    "url": "/static/media/gradient-background-generator.21761128.jpg"
+  },
+  {
+    "revision": "06ec3d3dc7defacc47cfd40f164299a8",
+    "url": "/static/media/making-pdfs-aoda-compliant.06ec3d3d.jpg"
+  },
+  {
+    "revision": "3c7e02ad59e3afb22d0f9e60f0e4dee7",
+    "url": "/static/media/outpost-magazine-shop.3c7e02ad.jpg"
+  },
+  {
+    "revision": "3698ac4c6e516338e2d3d73a4f5af1e3",
+    "url": "/static/media/daniel-puiatti-portfolio.3698ac4c.jpg"
+  },
+  {
+    "revision": "8258198fe88d23188a78bd1bd57d0c7a",
+    "url": "/static/media/identifying-a-target-audience-2.8258198f.jpg"
+  },
+  {
+    "revision": "990e3d32daec91ace05e",
+    "url": "/static/js/main.990e3d32.chunk.js"
+  },
+  {
+    "revision": "b7a50ceb26242dee0e1fd7c8a96245aa",
+    "url": "/static/media/starting-a-brand-from-scratch-1.b7a50ceb.jpg"
+  },
+  {
+    "revision": "a4adfbc7afbcdbf4bcde426d9d12e8a0",
+    "url": "/static/media/starting-a-brand-from-scratch-2.a4adfbc7.jpg"
+  },
+  {
+    "revision": "c7fb021d7c8ead1e6adda0b3445f66b9",
+    "url": "/static/media/identifying-a-target-audience-3.c7fb021d.jpg"
+  },
+  {
+    "revision": "5e1bb5061ec37992f97ce134e73f7b65",
+    "url": "/static/media/identifying-a-target-audience-4.5e1bb506.jpg"
+  },
+  {
+    "revision": "1dd9a2fd91c4e4aed9be219552c35ac5",
+    "url": "/static/media/reaching-a-target-audience-with-content-marketing-1.1dd9a2fd.jpg"
+  },
+  {
+    "revision": "2c06a456aaccd12ac6dc817e39c14b95",
+    "url": "/static/media/message.2c06a456.png"
+  },
+  {
+    "revision": "a2e107655c62228c9de44fef5d70256a",
+    "url": "/static/media/twitter.a2e10765.png"
+  },
+  {
+    "revision": "2ebfacc5e439dd78c1863a37786c636e",
+    "url": "/static/media/github.2ebfacc5.png"
+  },
+  {
+    "revision": "4c8db68feb0483e8e81834749c7ae2d9",
+    "url": "/static/media/web-development.4c8db68f.png"
+  },
+  {
+    "revision": "8f1d074446795a74016d6ce99d503e80",
+    "url": "/static/media/seo.8f1d0744.png"
+  },
+  {
+    "revision": "8e41d5598e706ecec706e59f78365edf",
+    "url": "/static/media/social-media.8e41d559.png"
+  },
+  {
+    "revision": "881b814eb582032c7c5d3128f2107fe1",
+    "url": "/static/media/copy-writing.881b814e.png"
+  },
+  {
+    "revision": "7a2b4f2f001915ef4e3b29f30c217b6e",
+    "url": "/index.html"
+  }
+];
